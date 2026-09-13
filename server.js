6d727f9aef2b95b90c1aa136298c7f54b146f7eb
Codydev7 <cody7dev@gmail.com>
2026-09-13 13:02:05 -0400

    server.js

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 const http = require('http');
-const WebSocket = require('ws');
 
-console.log('WS PRUEBA');
+console.log('INICIO');
+
+try {
+    const WebSocket = require('ws');
+    console.log('WS INSTALADO');
+} catch (error) {
+    console.error('ERROR CARGANDO WS');
+    console.error(error);
+}
 
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('WS TEST OK');
-});
-
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', ws => {
-    console.log('WEBSOCKET CONECTADO');
-
-    ws.send('Hola desde WebSocket');
+    res.end('TEST');
 });
 
 server.listen(PORT, '0.0.0.0', () => {
-    console.log(`PUERTO ABIERTO: ${PORT}`);
+    console.log('PUERTO ABIERTO:', PORT);
 });
