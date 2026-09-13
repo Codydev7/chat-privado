26db5d5adedbdf6e4e7bf08cf50f5507b4e31a01
Codydev7 <cody7dev@gmail.com>
2026-09-13 12:58:46 -0400

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const http = require('http');
+const WebSocket = require('ws');
 
-console.log('PRUEBA SERVER.JS');
+console.log('WS PRUEBA');
 
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
+    res.writeHead(200);
+    res.end('WS TEST OK');
+});
 
-    res.end('SERVIDOR DE PRUEBA FUNCIONANDO');
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', ws => {
+    console.log('WEBSOCKET CONECTADO');
+
+    ws.send('Hola desde WebSocket');
 });
 
 server.listen(PORT, '0.0.0.0', () => {
